822fae9ff75542ef43d3df4774528537dcf70e85
yc2002 <78285150+yc2002@users.noreply.github.com>
2023-07-17 23:27:54 +0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,14 @@
+import fasttext
+from typing import List, Tuple
+import re
+import pandas as pd
+
+# Load pre-trained language model
+MODEL = fasttext.load_model('lid.176.bin')
+
+# Pre-compiled regular expression to split on " - " or parentheses containing non-digits
+SPLITTER = re.compile(" - | \((?!\d+\)).*?\)")
+
 def extract_titles(titles: list[str]) -> list[tuple[str, str]]:
     """
     Args:
@@ -7,7 +18,24 @@ def extract_titles(titles: list[str]) -> list[tuple[str, str]]:
         english_titles (list): List of english titles.
     """
     # Your code here
-    non_english_titles = []
-    english_titles = []
-    return non_english_titles, english_titles
+    result = []
+    for title in titles:
+        parts = [part for part in SPLITTER.split(title) if part and any(c.isalpha() for c in part)]
 
+        # Initialize English and Non-English title variables as None
+        english_title, non_english_title = None, None
+
+        for part in parts:
+            # Predict the part's language using the FastText model
+            part_lang, _ = MODEL.predict(part, k=1)
+
+            # If the part is in English, set it as the English title
+            if '__label__en' in part_lang:
+                english_title = part if english_title is None else title #If both parts are in English, use the entire title
+            # If the part is not in English, set it as the Non-English title
+            else:
+                non_english_title = part if non_english_title is None else title #Vice versa
+
+        result.append((english_title, non_english_title))
+
+    return result
